7b4d630a7ad3cc6c68b6c2d3a7a6f6becdc6e7b0
Gilad Rom <giladrom@Gilads-MacBook-Pro.local>
2018-03-08 15:53:21 -0800

    Fix for Facebook plugin

---
+++ hooks/after_platform_add/facebook4-fix.sh
@@ -0,0 +1,12 @@
+if [ -d "platforms/android" ]; then
+  echo '
+configurations.all {
+    resolutionStrategy.eachDependency { DependencyResolveDetails details ->
+        def requested = details.requested
+        if (requested.group == "com.facebook.android") {
+          details.useVersion "4.25.0"
+        }
+    }
+}
+' > platforms/android/build-extras.gradle
+fi
