48dbe412f40e06ab23921ca8979588fc7baee1f2
Gilad Rom <giladrom@Gilads-MacBook-Pro.local>
2018-05-14 13:23:04 -0700

    Added image preloading

--- src/pages/found-pet/found-pet.module.ts
+++ src/pages/found-pet/found-pet.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { FoundPetPage } from './found-pet';
+import { IonicImageLoader } from 'ionic-image-loader';
 
 @NgModule({
-  declarations: [
-    FoundPetPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(FoundPetPage),
-  ],
+  declarations: [FoundPetPage],
+  imports: [IonicPageModule.forChild(FoundPetPage), IonicImageLoader]
 })
 export class FoundPetPageModule {}
