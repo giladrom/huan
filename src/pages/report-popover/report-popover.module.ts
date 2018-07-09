192ff09f293f976cc74cfe5e8a2af8a3fd8f500e
Gilad Rom <giladrom@Gilads-MacBook-Pro.local>
2018-07-09 13:14:11 -0700

    Use ImageLoader

--- src/pages/report-popover/report-popover.module.ts
+++ src/pages/report-popover/report-popover.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ReportPopoverPage } from './report-popover';
+import { IonicImageLoader } from 'ionic-image-loader';
 
 @NgModule({
-  declarations: [
-    ReportPopoverPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(ReportPopoverPage),
-  ],
+  declarations: [ReportPopoverPage],
+  imports: [IonicPageModule.forChild(ReportPopoverPage), IonicImageLoader]
 })
 export class ReportPopoverPageModule {}
