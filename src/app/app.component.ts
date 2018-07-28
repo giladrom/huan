eb985aa11fb5d9c1f3db0b0229b3e8a395ff7b91
Gilad Rom <giladrom@Gilads-MacBook-Pro.local>
2018-07-28 14:46:52 -0700

    Removed HockeyApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,6 @@ import { BleProvider } from '../providers/ble/ble';
 import { AuthProvider, UserAccount } from '../providers/auth/auth';
 import { SettingsProvider } from '../providers/settings/settings';
 
-import { HockeyApp } from 'ionic-hockeyapp';
 import { InitProvider } from '../providers/init/init';
 import { ImageLoaderConfig } from 'ionic-image-loader';
 import { Subscription } from 'rxjs/Subscription';
@@ -51,7 +50,6 @@ export class MyApp implements OnDestroy {
     private alertCtrl: AlertController,
     private menuCtrl: MenuController,
     private app: App,
-    private hockeyapp: HockeyApp,
     private init: InitProvider,
     private imageLoaderConfig: ImageLoaderConfig,
     private isDebug: IsDebug,
@@ -65,34 +63,14 @@ export class MyApp implements OnDestroy {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
 
-      // The Android ID of the app as provided by the HockeyApp portal. Can be null if for iOS only.
-      let androidAppId = '2fdddd2639634ea28e4be2e8b9de0a8a';
-      // The iOS ID of the app as provided by the HockeyApp portal. Can be null if for android only.
-      let iosAppId = 'd0ba634806634070a0e912961d5c564a';
-      // Specifies whether you would like crash reports to be automatically sent to the HockeyApp server when the end user restarts the app.
-      let autoSendCrashReports = false;
-      // Specifies whether you would like to display the standard dialog when the app is about to crash. This parameter is only relevant on Android.
-      let ignoreCrashDialog = true;
-
-      this.hockeyapp
-        .start(androidAppId, iosAppId, autoSendCrashReports, ignoreCrashDialog)
-        .then(data => {
-          console.info('HockeyApp started: ' + JSON.stringify(data));
-        })
-        .catch(error => {
-          console.error('HockeyApp error:' + JSON.stringify(error));
-        });
-
-      //So app doesn't close when hockey app activities close
-      //This also has a side effect of unable to close the app when on the rootPage and using the back button.
-      //Back button will perform as normal on other pages and pop to the previous page.
+      // Register Android back button
       platform.registerBackButtonAction(() => {
         let view = this.nav.getActive();
 
         if (view.getNav().canGoBack()) {
           view.getNav().pop();
         } else {
-          // this.navCtrl.setRoot(this.rootPage);
+          platform.exitApp();
         }
       });
 
