02f79e702f0e0851926fb499df1b93adbb66c151
Gilad Rom <giladrom@Gilads-MacBook-Pro.local>
2018-09-13 16:52:53 +0300

    Add error handler

--- src/providers/tag/tag.ts
+++ src/providers/tag/tag.ts
@@ -539,28 +539,38 @@ export class TagProvider implements OnDestroy {
           var paddedId = this.utils.pad(tagId, 4, '0');
           var utc = Date.now().toString();
 
-          this.authProvider.getUserId().then(uid => {
-            tagCollectionRef
-              .doc(paddedId)
-              .update({
-                location: locationStr,
-                lastseen: utc,
-                lastseenBy: uid
-              })
-              .then(() => {
-                resolve(true);
-              })
-              .catch(error => {
-                console.error(
-                  'updateTagData:  Unable to update Tag ' +
-                    paddedId +
-                    ': ' +
-                    error
-                );
+          this.authProvider
+            .getUserId()
+            .then(
+              uid => {
+                tagCollectionRef
+                  .doc(paddedId)
+                  .update({
+                    location: locationStr,
+                    lastseen: utc,
+                    lastseenBy: uid
+                  })
+                  .then(() => {
+                    resolve(true);
+                  })
+                  .catch(error => {
+                    console.error(
+                      'updateTagData:  Unable to update Tag ' +
+                        paddedId +
+                        ': ' +
+                        error
+                    );
 
-                reject(error);
-              });
-          });
+                    reject(error);
+                  });
+              },
+              err => {
+                console.error('updateTagData: getUserId(): ' + err);
+              }
+            )
+            .catch(e => {
+              console.error('updateTagData: Unable to get user info: ' + e);
+            });
         })
         .catch(error => {
           console.error('updateTagData: Unable to get location: ' + error);
