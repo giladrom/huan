13deeaa2dedf66cceb4dff4900e979175c9a37ce
Gilad Rom <giladrom@Gilads-MacBook-Pro.local>
2018-11-01 12:09:01 -0700

    Create/update records

--- scripts/airtable_sync.js
+++ scripts/airtable_sync.js
@@ -21,45 +21,121 @@ db.settings(settings);
 
 // Get all records in database, iterate and replace if existing, create if not
 
-db.collection('Tags')
-  .get()
-  .then(snapshot => {
-    snapshot.forEach(doc => {
-      var tag = doc.data();
+var airtable_records = [];
 
-      console.log(tag);
-
-      base('Tags')
-        .create({
-          'Tag ID': Number(tag.tagId),
-          Name: tag.name,
-          Breed: typeof tag.breed === 'string' ? tag.breed : tag.breed[0],
-          Character: tag.character,
-          Color: typeof tag.color === 'string' ? tag.color : tag.color[0],
-          Gender: tag.gender,
-          Image: [{ url: tag.img }],
-          'Last Seen':
-            typeof tag.lastseen === 'string'
-              ? moment(Number(tag.lastseen)).format()
-              : moment(tag.lastseen.toDate()).format(),
-          'Last Seen By': tag.lastseenBy,
-          Location: tag.location,
-          Lost: tag.lost,
-          Remarks: tag.remarks,
-          Size: tag.size,
-          'Tag Attached': tag.tagattached,
-          User: typeof tag.uid === 'string' ? tag.uid : tag.uid[0]
-        })
-        .then(record => {
-          console.log(record.getId());
-        })
-        .catch(e => {
-          console.error('ERROR: ' + e);
-        });
-    });
-
-    // tags.sort((a, b) => a.id < b.id);
+base('Tags')
+  .select({
+    // Selecting the first 3 records in Grid view:
+    // maxRecords: 100,
+    fields: ['Tag ID']
   })
-  .catch(err => {
-    console.log('Error getting documents', err);
-  });
+  .eachPage(
+    function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+
+      records.forEach(function(record) {
+        // console.log('Retrieved', record.getId(), record.get('Tag ID'));
+        airtable_records.push({
+          record_id: record.getId(),
+          tagId: record.get('Tag ID')
+        });
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+    },
+    function done(err) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      db.collection('Tags')
+        .get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            var tag = doc.data();
+            var r = airtable_records.findIndex(value => {
+              return value.tagId == tag.tagId;
+            });
+
+            if (r > -1) {
+              console.log(
+                `${tag.tagId} has a record ID of ${
+                  airtable_records[r].record_id
+                }`
+              );
+
+              base('Tags')
+                .replace(airtable_records[r].record_id, {
+                  'Tag ID': Number(tag.tagId),
+                  Name: tag.name,
+                  Breed:
+                    typeof tag.breed === 'string' ? tag.breed : tag.breed[0],
+                  Character: tag.character,
+                  Color:
+                    typeof tag.color === 'string' ? tag.color : tag.color[0],
+                  Gender: tag.gender,
+                  Image: [{ url: tag.img }],
+                  'Last Seen':
+                    typeof tag.lastseen === 'string'
+                      ? moment(Number(tag.lastseen)).format()
+                      : moment(tag.lastseen.toDate()).format(),
+                  'Last Seen By': tag.lastseenBy,
+                  Location: tag.location,
+                  Lost: tag.lost,
+                  Remarks: tag.remarks,
+                  Size: tag.size,
+                  'Tag Attached': tag.tagattached,
+                  User: typeof tag.uid === 'string' ? tag.uid : tag.uid[0]
+                })
+                .then(record => {
+                  console.log('Replace: OK: ', record.getId());
+                })
+                .catch(e => {
+                  console.error('Replace: ERROR: ' + e);
+                });
+            } else {
+              base('Tags')
+                .create({
+                  'Tag ID': Number(tag.tagId),
+                  Name: tag.name,
+                  Breed:
+                    typeof tag.breed === 'string' ? tag.breed : tag.breed[0],
+                  Character: tag.character,
+                  Color:
+                    typeof tag.color === 'string' ? tag.color : tag.color[0],
+                  Gender: tag.gender,
+                  Image: [{ url: tag.img }],
+                  'Last Seen':
+                    typeof tag.lastseen === 'string'
+                      ? moment(Number(tag.lastseen)).format()
+                      : moment(tag.lastseen.toDate()).format(),
+                  'Last Seen By': tag.lastseenBy,
+                  Location: tag.location,
+                  Lost: tag.lost,
+                  Remarks: tag.remarks,
+                  Size: tag.size,
+                  'Tag Attached': tag.tagattached,
+                  User: typeof tag.uid === 'string' ? tag.uid : tag.uid[0]
+                })
+                .then(record => {
+                  console.log('Create: OK: ', record.getId());
+                })
+                .catch(e => {
+                  console.error('Create: ERROR: ' + e);
+                });
+            }
+          });
+        })
+        .catch(err => {
+          console.log('Error getting documents', err);
+        });
+
+      //   airtable_records.forEach(record => {
+      //     console.log(`Record: ${record.record_id} Tag ID: ${record.tagId}`);
+      //   });
+    }
+  );
