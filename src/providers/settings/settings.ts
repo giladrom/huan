c397235f1ec137af381c583a45d7ff3b35cbc0f5
Gilad Rom <giladrom@Gilads-MacBook-Pro.local>
2018-12-31 23:32:11 -0800

    Remove comments

--- src/providers/settings/settings.ts
+++ src/providers/settings/settings.ts
@@ -200,17 +200,6 @@ export class SettingsProvider implements OnDestroy {
 
   getSettings(): BehaviorSubject<Settings> {
     return this.settings$;
-
-    // return new Promise<any>(async (resolve, reject) => {
-    //   try {
-    //     await this.loadSettings();
-    //   } catch (error) {
-    //     console.error('getSettings: error: ' + JSON.stringify(error));
-    //     reject(error);
-    //   }
-
-    //   resolve(this.settings);
-    // });
   }
 
   setRegionNotifications(value: boolean) {
@@ -271,66 +260,6 @@ export class SettingsProvider implements OnDestroy {
       setRef.update({ 'settings.shareContactInfo': value });
     });
   }
-
-  // XXX Ionic Pro
-  // FIXME: Make this shit work
-
-  // async checkChannel() {
-  //   try {
-  //     const res = await Pro.deploy.getConfiguration();
-  //     this.deployChannel = res.channel;
-  //     this.isBeta = this.deployChannel === 'Beta';
-  //   } catch (err) {
-  //     // We encountered an error.
-  //     // Here's how we would log it to Ionic Pro Monitoring while also catching:
-  //     // Pro.monitoring.exception(err);
-  //   }
-  // }
-
-  // async toggleBeta() {
-  //   const config = {
-  //     channel: this.isBeta ? 'Beta' : 'Production'
-  //   };
-
-  //   try {
-  //     await Pro.deploy.configure(config);
-  //     await this.checkChannel();
-  //     await this.sync({ updateMethod: 'auto' }); // Alternatively, to customize how this works, use performManualUpdate()
-  //   } catch (err) {
-  //     // We encountered an error.
-  //     // Here's how we would log it to Ionic Pro Monitoring while also catching:
-  //     // Pro.monitoring.exception(err);
-  //   }
-  // }
-
-  // async performManualUpdate() {
-  //   /*
-  //     Here we are going through each manual step of the update process:
-  //     Check, Download, Extract, and Redirect.
-
-  //     Ex: Check, Download, Extract when a user logs into your app,
-  //       but Redirect when they logout for an app that is always running
-  //       but used with multiple users (like at a doctors office).
-  //   */
-
-  //   try {
-  //     const update = await Pro.deploy.checkForUpdate();
-
-  //     if (update.available) {
-  //       this.downloadProgress = 0;
-
-  //       await Pro.deploy.downloadUpdate(progress => {
-  //         this.downloadProgress = progress;
-  //       });
-  //       await Pro.deploy.extractUpdate();
-  //       await Pro.deploy.reloadApp();
-  //     }
-  //   } catch (err) {
-  //     // We encountered an error.
-  //     // Here's how we would log it to Ionic Pro Monitoring while also catching:
-  //     // Pro.monitoring.exception(err);
-  //   }
-  // }
 
   ngOnDestroy() {
     // this.authSubscription.unsubscribe();
