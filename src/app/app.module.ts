d9117ba5d10d2aac1699640770b8fa42e578b416
Gilad Rom <giladrom@Gilads-MacBook-Pro.local>
2019-01-01 21:40:24 -0800

    Bumped app version to 1.0.8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -91,7 +91,7 @@ import { ProgressBarModule } from 'angular-progress-bar';
 import { BranchIo } from '@ionic-native/branch-io';
 
 Pro.init('abdad7ef', {
-  appVersion: '1.0.7'
+  appVersion: '1.0.8'
 });
 
 @Injectable()
